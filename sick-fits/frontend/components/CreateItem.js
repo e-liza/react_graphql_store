494904eb9bfc8084306d2c857c5f4f730d718fdc
e-liza <eliza.lks@gmail.com>
2019-04-23 09:31:23 +0200

    Adding update item functionality

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -29,8 +29,8 @@ class CreateItem extends Component {
   state = {
     title: 'Cool Shoes',
     description: 'I love those shoes',
-    image: 'dog.jpg',
-    largeImage: 'large-dog.jpg',
+    image: '',
+    largeImage: '',
     price: 1000
   };
   handleChange = e => {
@@ -38,6 +38,25 @@ class CreateItem extends Component {
     const val =
       type === 'number' ? parseFloat(value) : value;
     this.setState({ [name]: val });
+  };
+
+  uploadFile = async e => {
+
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/lizzard/image/upload', {
+      method: 'POST',
+      body: data,
+    });
+    const file = await res.json();
+
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    });
   };
   render() {
     return (
@@ -68,6 +87,20 @@ class CreateItem extends Component {
               disabled={loading}
               aria-busy={loading}
             >
+            <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && (
+                  <img width="200" src={this.state.image} alt="Upload Preview" />
+                )}
+              </label>
               <label htmlFor="title">
                 Title
                 <input
