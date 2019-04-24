1dd9cac5ce68bd907646d7aa8c55e668ce967736
e-liza <eliza.lks@gmail.com>
2019-04-24 12:13:32 +0200

    Adding single item page component

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -28,24 +28,22 @@ app
     });
 
     server.start(
-        {
-          cors: {
-            credentials: true,
-            origin: process.env.FRONTEND_URL
-          }
-        },
-        deets => {
-          console.log(
-            `Server is now running on port http:/localhost:${
-              deets.port
-            }`
-          );
+      {
+        cors: {
+          credentials: true,
+          origin: process.env.FRONTEND_URL
         }
-      );
+      },
+      deets => {
+        console.log(
+          `Server is now running on port http:/localhost:${
+            deets.port
+          }`
+        );
+      }
+    );
   })
   .catch(ex => {
     console.error(ex.stack);
     process.exit(1);
   });
-
-
