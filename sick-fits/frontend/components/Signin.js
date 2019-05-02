9232c39c72c22c7011c3d6ddb5ce4f733d857f03
e-liza <eliza.lks@gmail.com>
2019-05-02 10:05:21 +0200

    Sign in functionality on frontend

--- sick-fits/frontend/components/Signin.js
+++ sick-fits/frontend/components/Signin.js
@@ -3,20 +3,11 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import Form from './styles/Form';
 import Error from './ErrorMessage';
-
 import { CURRENT_USER_QUERY } from './User';
 
-const SIGNUP_MUTATION = gql`
-  mutation SIGNUP_MUTATION(
-    $email: String!
-    $name: String!
-    $password: String!
-  ) {
-    signup(
-      email: $email
-      name: $name
-      password: $password
-    ) {
+const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    signin(email: $email, password: $password) {
       id
       email
       name
@@ -24,11 +15,10 @@ const SIGNUP_MUTATION = gql`
   }
 `;
 
-class Signup extends Component {
+class Signin extends Component {
   state = {
-    name: '',
     password: '',
-    email: ''
+    email: '',
   };
   saveToState = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -36,28 +26,21 @@ class Signup extends Component {
   render() {
     return (
       <Mutation
-        mutation={SIGNUP_MUTATION}
+        mutation={SIGNIN_MUTATION}
         variables={this.state}
         refetchQueries={[{ query: CURRENT_USER_QUERY }]}
       >
-        {(signup, { error, loading }) => (
+        {(signin, { error, loading }) => (
           <Form
             method="post"
             onSubmit={async e => {
               e.preventDefault();
-              await signup();
-              this.setState({
-                name: '',
-                email: '',
-                password: ''
-              });
+              await signin();
+              this.setState({ email: '', password: '' });
             }}
           >
-            <fieldset
-              disabled={loading}
-              aria-busy={loading}
-            >
-              <h2>Sign Up for An Account</h2>
+            <fieldset disabled={loading} aria-busy={loading}>
+              <h2>Sign into your account</h2>
               <Error error={error} />
               <label htmlFor="email">
                 Email
@@ -66,16 +49,6 @@ class Signup extends Component {
                   name="email"
                   placeholder="email"
                   value={this.state.email}
-                  onChange={this.saveToState}
-                />
-              </label>
-              <label htmlFor="name">
-                Name
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="name"
-                  value={this.state.name}
                   onChange={this.saveToState}
                 />
               </label>
@@ -90,7 +63,7 @@ class Signup extends Component {
                 />
               </label>
 
-              <button type="submit">Sign Up!</button>
+              <button type="submit">Sign In!</button>
             </fieldset>
           </Form>
         )}
@@ -99,4 +72,4 @@ class Signup extends Component {
   }
 }
 
-export default Signup;
+export default Signin;
