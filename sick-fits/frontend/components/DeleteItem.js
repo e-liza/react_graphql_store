a6ebb38ae42fc79b583dbd09ee6d6c912e584406
e-liza <eliza.lks@gmail.com>
2019-05-15 13:06:57 +0200

    Sending updated permissions to backend on button click or on change of the input field. Error handling on attempt of deleting an item without required permissions.

--- sick-fits/frontend/components/DeleteItem.js
+++ sick-fits/frontend/components/DeleteItem.js
@@ -15,10 +15,13 @@ class DeleteItem extends Component {
   update = (cache, payload) => {
     // manually update the cache on the client, so it matches the server
     // 1. Read the cache for the items we want
-    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
-    console.log(data, payload);
-    // 2. Filter the deleted item out of the page
-    data.items = data.items.filter(item => item.id !== payload.data.deleteItem.id);
+    const data = cache.readQuery({
+      query: ALL_ITEMS_QUERY
+    });
+    // 2. Filter the deleted itemout of the page
+    data.items = data.items.filter(
+      item => item.id !== payload.data.deleteItem.id
+    );
     // 3. Put the items back!
     cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
   };
@@ -32,8 +35,14 @@ class DeleteItem extends Component {
         {(deleteItem, { error }) => (
           <button
             onClick={() => {
-              if (confirm('Are you sure you want to delete this item?')) {
-                deleteItem();
+              if (
+                confirm(
+                  'Are you sure you want to delete this item?'
+                )
+              ) {
+                deleteItem().catch(err => {
+                  alert(err.message);
+                });
               }
             }}
           >
