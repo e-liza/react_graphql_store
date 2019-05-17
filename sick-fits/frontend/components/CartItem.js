3a240bf9a627b5121cd61d37fd47e768855bbe18
e-liza <eliza.lks@gmail.com>
2019-05-17 11:23:41 +0200

    Remove item from cart (server side) on front end

--- sick-fits/frontend/components/CartItem.js
+++ sick-fits/frontend/components/CartItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import formatMoney from '../lib/formatMoney';
+import RemoveFromCart from './RemoveFromCart';
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -20,22 +21,30 @@ const CartItemStyles = styled.li`
 
 const CartItem = ({ cartItem }) => (
   <CartItemStyles>
-    <img width="100" src={cartItem.item.image} alt={cartItem.item.title} />
+    <img
+      width="100"
+      src={cartItem.item.image}
+      alt={cartItem.item.title}
+    />
     <div className="cart-item-details">
       <h3>{cartItem.item.title}</h3>
       <p>
-        {formatMoney(cartItem.item.price * cartItem.quantity)}
+        {formatMoney(
+          cartItem.item.price * cartItem.quantity
+        )}
         {' - '}
         <em>
-          {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
+          {cartItem.quantity} &times;{' '}
+          {formatMoney(cartItem.item.price)} each
         </em>
       </p>
     </div>
+    <RemoveFromCart id={cartItem.id} />
   </CartItemStyles>
 );
 
 CartItem.propTypes = {
-  cartItem: PropTypes.object.isRequired,
+  cartItem: PropTypes.object.isRequired
 };
 
 export default CartItem;
