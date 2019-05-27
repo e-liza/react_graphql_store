9d6a49727a23673865c4f47ba1d97c7fc7209cf2
e-liza <eliza.lks@gmail.com>
2019-05-27 19:26:19 +0200

    Adding an animated cart item counter to navigation

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -3,6 +3,7 @@ import { Mutation } from 'react-apollo';
 import { TOGGLE_CART_MUTATION } from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
+import CartCount from './CartCount';
 import Signout from './Signout';
 
 const Nav = () => (
@@ -25,9 +26,10 @@ const Nav = () => (
             </Link>
             <Signout />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {toggleCart => (
+              {(toggleCart) => (
                 <button onClick={toggleCart}>
                   My Cart
+                  <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)}></CartCount>
                 </button>
               )}
             </Mutation>
@@ -37,6 +39,7 @@ const Nav = () => (
           <Link href="/signup">
             <a>Sign In</a>
           </Link>
+
         )}
       </NavStyles>
     )}
